03b00a5d027362602d743a0747206f074ec10665
Thuy-Linh Tran <153449781+linh-ttt@users.noreply.github.com>
2025-12-18 00:43:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -69,10 +69,7 @@ async def check_time(application):
         "07:55": "⏰ Sắp đến giờ làm việc rồi bạn ơi! Nhớ chấm công đầy đủ trước 8:00 nhé!",
         "12:00": "🍱 Đến giờ nghỉ trưa rồi bạn ơi!",
         "13:00": "⏰ Tạm biệt nghỉ trưa! Quay lại làm việc thôi!",
-        "17:00": "📤 Đến giờ tan làm rồi bạn ơi! Nhớ chấm công trước khi ra về nhé!",
-        "00:35": "Test lan 3",
-        "00:36": "Test lan 1",
-        "00:37": "Test lan 2"
+        "17:00": "📤 Đến giờ tan làm rồi bạn ơi! Nhớ chấm công trước khi ra về nhé!"
     }
 
     if time_now in messages:
